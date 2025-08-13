a3f5b90e71dc646f8766c03067b74328e3b576b4
AMir Nasimzada <amirnasimzada@macbook-pro.mynetworksettings.com>
2025-08-13 15:43:42 -0400

    update workflow

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,9 +2,9 @@
 terraform {
   required_version = ">= 1.6.6"
   backend "s3" {
-    bucket = "tf-state-amir-demo"
-    key    = "terraform-state/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "tf-state-amir-demo"
+    key     = "terraform-state/terraform.tfstate"
+    region  = "us-east-1"
     encrypt = true
   }
 }
