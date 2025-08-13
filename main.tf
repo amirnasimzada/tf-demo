be7f5a4a679a7b1b204af61ac2a100da2ef05b05
AMir Nasimzada <amirnasimzada@macbook-pro.mynetworksettings.com>
2025-08-13 15:22:50 -0400

    remove dup provider

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-# Configure the AWS provider
-provider "aws" {
-  region = "us-east-1"
-}
-
 # Create a S3 bucket
 resource "aws_s3_bucket" "terraform_state" {
   bucket		  = "test-s3-bucket-amir-nas"
